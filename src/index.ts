fd96502e1479cbbcac0da03a7a8c6630f9117e48
Yanislav Igonin <jan.oasis@gmail.com>
2019-10-09 22:56:07 +0300

    Add simple http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('all systems nominal');
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { redis } from './modules';
+import { app } from './config';
+
+const handler = async (req: IncomingMessage, res: ServerResponse): Promise<void> => {
+  if (req.method === 'GET') {
+    const key = req.url ? req.url.split('/')[1] : '';
+    const value = await redis.getAsync(key);
+    res.end(value);
+  }
+};
+
+const server = createServer(handler);
+
+server.listen(app.port, (): void => {
+  console.log(`server - online on ${app.port}`);
+  console.log('all systems nominal');
+});
