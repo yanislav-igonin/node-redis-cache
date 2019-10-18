78f05f0e8cacc30d3a8ad09aaaae841529fc99f8
Yanislav Igonin <jan.oasis@gmail.com>
2019-10-18 14:37:32 +0300

    Update cache module

--- src/modules/cache/index.ts
+++ src/modules/cache/index.ts
@@ -2,13 +2,12 @@ import redis from '../redis';
 import { cache as cacheConfig } from '../../config';
 
 const cache = new Map();
-const cacheHitCount: {[key: string]: number} = {};
-const cacheExpiration: {[key: string]: NodeJS.Timeout} = {};
-let currentCacheFullness = 0;
+const cacheHitCount = new Map();
+const cacheExpiration = new Map();
 let minUsedCacheKey: string;
 
 const findMinUsedCacheKey = (): string => {
-  const values = Object.entries(cacheHitCount);
+  const values = Array.from(cacheHitCount);
 
   const sorted = values.sort((a, b) => a[1] - b[1]);
   console.log('DEBUG: sorted', sorted);
@@ -17,37 +16,34 @@ const findMinUsedCacheKey = (): string => {
 };
 
 const remove = (key: string): void => {
-  console.log(`remove -> key: ${key}`);
+  // console.log(`remove -> key: ${key}`);
   cache.delete(key);
-  delete cacheHitCount[key];
-  clearTimeout(cacheExpiration[key]);
-  delete cacheExpiration[key];
-  currentCacheFullness -= 1;
+  cacheHitCount.delete(key);
+  clearTimeout(cacheExpiration.get(key));
+  cacheExpiration.delete(key);
 };
 
 const update = (key: string): void => {
-  console.log(`update -> key: ${key}`);
-  cacheExpiration[key].refresh();
-  cacheHitCount[key] += 1;
+  // console.log(`update -> key: ${key}`);
+  cacheExpiration.get(key).refresh();
+  cacheHitCount.set(key, cacheHitCount.get(key) + 1);
 };
 
 const add = async (key: string, value: string): Promise<void> => {
-  console.log(`add -> key: ${key}, value: ${value}`);
-  if (currentCacheFullness >= cacheConfig.capacity) {
-    console.log('cache full');
+  // console.log(`add -> key: ${key}, value: ${value}`);
+  if (cache.size >= cacheConfig.capacity) {
+    // console.log('cache full');
     minUsedCacheKey = findMinUsedCacheKey();
-    console.log('DEBUG: minUsedCacheKey', minUsedCacheKey);
     remove(minUsedCacheKey);
   }
 
   cache.set(key, value);
-  cacheHitCount[key] = 1;
-  cacheExpiration[key] = setTimeout(() => remove(key), cacheConfig.expireTime);
-  currentCacheFullness += 1;
+  cacheHitCount.set(key, 1);
+  cacheExpiration.set(key, setTimeout(() => remove(key), cacheConfig.expireTime));
 };
 
 const get = async (key: string): Promise<string | null> => {
-  console.log(`get -> key: ${key}`);
+  // console.log(`get -> key: ${key}`);
   let value = cache.get(key);
 
   if (value === undefined) {
@@ -60,7 +56,7 @@ const get = async (key: string): Promise<string | null> => {
     update(key);
   }
 
-  console.log('==========================');
+  // console.log('==========================');
 
   return value;
 };
